442576d164849e7b6e9d811e10c17e10a10a31fe
Darang00 <201901427a@inu.ac.kr>
2023-02-01 11:57:59 +0900

    230201morning

---
+++ src/main/java/kr/or/dummys/dao/UserDao.java
@@ -0,0 +1,22 @@
+package kr.or.dummys.dao;
+
+import kr.or.dummys.dto.Users;
+
+public interface UserDao {
+	
+	//회원가입
+	public int insertUser(Users users);
+	
+	//아이디 체크
+	public int idCheck(String id);
+	
+	//로그인 체크
+	public int loginCheck(String id, String password);
+	
+	//회원 상세
+	public Users userDetail(String id);
+	
+	//회원 수정
+	public int updateUser(Users users);
+
+}
---
+++ src/main/java/kr/or/dummys/service/user/UserService.java
@@ -0,0 +1,8 @@
+package kr.or.dummys.service.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
