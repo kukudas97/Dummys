1c9f5697288a5b6c7cfa5896694e39f4f8f66429
kukudas <zlawoghd@gmail.com>
2023-02-01 16:49:10 +0900

    security_clear_kukudas

--- src/main/java/kr/or/dummys/controller/UsersController.java
+++ src/main/java/kr/or/dummys/controller/UsersController.java
@@ -3,51 +3,60 @@ package kr.or.dummys.controller;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.or.dummys.dto.Users;
 import kr.or.dummys.service.user.UserService;
 
 
 
 @Controller
-@RequestMapping("/users")
+@RequestMapping("/users/*")
 public class UsersController {
 	
 	@Autowired
 	private UserService userService;
 	
-	//로그인 처리화면
-	/*
-	@RequestMapping(value="login.do", method=RequestMethod.GET)
-	public String userConfirm() {
-		return "users/login";
-	}*/
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
+	
+	//로그인 처리화면
+
 	@GetMapping("/login.do")
 	public String login() {
 
 		return "users/login";
 	}
 	
-	/*
 	@PostMapping("/login.do")
-	public String login(Principal pri) {
+	public String loginOK(@RequestParam("password") String rawPassword, Principal pri) {
+		String viewpage = "";
+		System.out.println(rawPassword);
 		
-		System.out.println(pri.getName());
-		System.out.println("안녕");
-		return "redirect:/admin/test";
+		//회원정보
+		Users user = userService.getUser(pri.getName());
+		
+		String encodedPassword = user.getPassword();
+		
+		boolean result = bCryptPasswordEncoder.matches(rawPassword, encodedPassword);
+		
+		if(result) {
+			viewpage = "redirect:join.do";
+		}else {
+			viewpage = "redirect:index.do";
+		}
+		
+		return viewpage;
 	}
-	*/
 	
-	@GetMapping("/logout.do")
-	public String logout() {
-		return "home";
-	}
-	
+	//error
 	@GetMapping("/error.do")
 	public String error() {
 		return "error";
--- src/main/java/kr/or/dummys/dao/UserDao.java
+++ src/main/java/kr/or/dummys/dao/UserDao.java
@@ -6,17 +6,16 @@ public interface UserDao {
 	
 	//회원가입
 	public int insertUser(Users users);
-/*	
+	
 	//아이디 체크
-	public int idCheck(String id);
+	public int idCheck(String userid);
 	
 	//로그인 체크
-	public int loginCheck(String id, String password);
+	public int loginCheck(String userid, String password);
 	
 	//회원 상세
-	public Users userDetail(String id);
+	public Users userDetail(String userid);
 	
 	//회원 수정
 	public int updateUser(Users users);
-*/
 }
