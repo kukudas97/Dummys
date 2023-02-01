5698b9c5c59831b5d09b322f325a160060d0eee7
kukudas <zlawoghd@gmail.com>
2023-02-01 09:02:14 +0900

    security_ing_kuku

--- src/main/java/kr/or/dummys/controller/UsersController.java
+++ src/main/java/kr/or/dummys/controller/UsersController.java
@@ -2,6 +2,7 @@ package kr.or.dummys.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,9 +16,21 @@ public class UsersController {
 	private UsersService usersService;
 	
 	//로그인 처리화면
+	/*
 	@RequestMapping(value="login.do", method=RequestMethod.GET)
 	public String userConfirm() {
 		return "users/login";
+	}*/
+	
+	@GetMapping("/login.do")
+	public String login() {
+		System.out.println("들어옴??");
+		return "users/login";
+	}
+	
+	@GetMapping("/logout.do")
+	public String logout() {
+		return null;
 	}
 
 }
