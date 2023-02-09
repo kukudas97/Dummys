606f9203ed53fc5058fac7e112eca893a4dd5484
TaeWooKim <dhkdtpdn@gmail.com>
2023-02-09 22:54:41 +0900

    김태우 트랜잭션처리,  정규분포 완성

--- src/main/java/kr/or/dummys/dao/GaussianDao.java
+++ src/main/java/kr/or/dummys/dao/GaussianDao.java
@@ -14,4 +14,12 @@ public interface GaussianDao {
 	public List<Gaussian> getGaussianListById(String userid);
 	
 	public int deleteGaussianByNo(int no);
+	
+	public Gaussian getGaussianByNo(String gaussian_no);
+	
+	public List<Gaussian_result> getGaussianResultListByNo(String gaussian_no);
+	
+	public int deleteGaussianResultByGaussianNo(int gaussian_no);
+	
+	public int updateGaussian(Gaussian gaussian);
 }
