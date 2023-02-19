9089d9c348bee7f62a03fa20ea76ba3faa63c23f
kimchansooo <kcs50604914@gmail.com>
2023-02-19 22:57:31 +0900

    2/19 김찬수 messageService

--- src/main/java/kr/or/dummys/service/message/MessageService.java
+++ src/main/java/kr/or/dummys/service/message/MessageService.java
@@ -1,12 +1,15 @@
 package kr.or.dummys.service.message;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.or.dummys.dao.BoardDao;
 import kr.or.dummys.dao.MessageDao;
 import kr.or.dummys.dao.UserDao;
 import kr.or.dummys.dto.Message;
@@ -17,13 +20,29 @@ public class MessageService {
 	@Autowired
 	private SqlSession sqlsession;
 	
-	public List<Message> messageList(){
+	
+	//보내고 받은 메세지 리스트
+	public Map<String, Object> myMessageList(String userid){
 		
-		List<Message> messageList = null;
+		Map<String, Object> mymessageMap = new HashMap<String, Object>();
+		List<Message> myMessagesendList = null;
+		List<Message> myMessagereceiveList = null;
+		
+		try {
+			MessageDao messageDao = sqlsession.getMapper(MessageDao.class);
+			myMessagesendList=messageDao.myMessagesendList(userid);
+			myMessagereceiveList = messageDao.myMessagereceiveList(userid);
+			
+			mymessageMap.put("myMessagesendList", myMessagesendList);
+			mymessageMap.put("myMessagereceiveList", myMessagereceiveList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		
-		return null;
+		return mymessageMap;
 	}
+	
 	
 	//DB에 동일한 아이디 있나 확인
 	public int idcheck(String id) throws ClassNotFoundException, SQLException {
@@ -35,9 +54,13 @@ public class MessageService {
 		return cnt;
 	}
 	//메세지 쓰기 처리
-	public String messageWrite() {
+	public int messageWrite(Message message) throws ClassNotFoundException, SQLException {
 		
-		return null;
+		System.out.println("메세지 처리 서비스다");
+		int result = 0;
+		MessageDao messagedao = sqlsession.getMapper(MessageDao.class);
+		result = messagedao.messageWrite(message);
+		return result;
 	}
 	
 }
